958678764c5cd1563949cd32f98f6fce4028aad2
Paolo <p.bucci.wh@gmail.com>
2023-10-08 21:00:31 +0200

    mock commit

--- repositories/papers-repository.js
+++ repositories/papers-repository.js
@@ -2,20 +2,20 @@ const {Paper} = require('../models/papers-model');
 const {dbConn , closeConn} = require ('../db-connection');
 
 async function save(newPaper){
-    dbConn()
+    await dbConn()
     paper = new Paper(newPaper)
     await paper.save(paper)
     closeConn()
 }
 async function getById(id){
-    dbConn()
+    await dbConn()
     const paper =  await Paper.findOne({_id : id})
     closeConn()
     return paper
  
 }
 async function getAll(){
-    dbConn()
+    await dbConn()
     const paper = await Paper.find()
     closeConn()
     return paper
